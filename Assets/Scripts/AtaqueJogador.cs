dff33c6e3a7ba4db0544e93d8508c355a213dfd7
ayyx3 <beatriz.almeida.1199@gmail.com>
2025-11-06 12:09:28 -0300

    Algumas mudanças antes da prova

--- Assets/Scripts/AtaqueJogador.cs
+++ Assets/Scripts/AtaqueJogador.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AtaqueJogador : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] int danoJogador = 10;
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.CompareTag("Inimigo"))
+        {
+            SistemaDeVidaInimigo sistemaDeVidaInimigo = other.gameObject.GetComponent<SistemaDeVidaInimigo>();
+            sistemaDeVidaInimigo.AplicarDano(danoJogador);
+        }
     }
 }
