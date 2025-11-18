7c304a0696f2a7a8c5d11abcd6b0d324b9689aaa
MuriloC03 <murilocarassato@gmail.com>
2025-11-18 15:15:57 -0300

    Protótipo

--- Assets/Scripts/BarraDeVida.cs
+++ Assets/Scripts/BarraDeVida.cs
@@ -7,7 +7,9 @@ public class BarraDeVida : MonoBehaviour
 
     public void AtualizarUI(float calculoVida)
     {
-        Vector3 novaEscala = new Vector3(calculoVida, 1, 1);
-        vidaAtualImage.transform.localScale = novaEscala;
+        if (calculoVida >= 0) {
+            Vector3 novaEscala = new Vector3(calculoVida, 1, 1);
+            vidaAtualImage.transform.localScale = novaEscala;
+            }
+        }
     }
-}
